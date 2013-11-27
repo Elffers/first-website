d77128c1f6c578344aa253c4a69f383076e898a1
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-11-27 15:21:05 -0800

    added date

--- my_app.rb
+++ my_app.rb
@@ -1,8 +1,16 @@
 # my_app.rb 
 
 require 'sinatra'
+require 'yaml'
 
 class MyApp < Sinatra::Base
+
+  before do
+   @posts = Dir.glob("views/posts/*.erb").map do |post_name| 
+      post_name.split("/").last.slice(0..-5)
+    end
+  end
+
   get '/' do
     erb :index
   end
@@ -23,12 +31,33 @@ class MyApp < Sinatra::Base
     erb :contact
   end
 
+  get '/blog' do
+    erb :blog
+  end
+
+
   get "/cute_pictures_of_animals" do
     erb :cute_pictures_of_animals
   end
 
   get "/blog/:post_name" do
-    erb "/posts/#{params[:post_name]}".to_sym
+    page = erb("/posts/#{params[:post_name]}".to_sym, layout: false)
+    page_body = page.split("\n\n", 2).last
+    erb page_body
+  end
+
+  def meta_data
+    if @metadata
+      @metadata
+    else 
+      @metadata = {}
+      @posts.each do |post|
+      html = erb("/posts/#{post}".to_sym, layout: false) #returns string with the post as a string
+      meta = YAML.load(html.split("\n\n", 2).first) #returns hash
+      metadata[post] = meta #puts hash into hash
+    end
+      @metadata
+    end
   end
 
 end

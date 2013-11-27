f86f56d9e76c4a8776ac9c40d39c8823e1e3a1d6
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-11-27 09:23:27 -0800

    basic layout

--- my_app.rb
+++ my_app.rb
@@ -3,12 +3,30 @@
 require 'sinatra'
 
 class MyApp < Sinatra::Base
-  get "/" do
+  get '/' do
     erb :index
+  end
+
+  get '/home' do
+    erb :index
+  end
+
+  get '/about' do
+    erb :about
+  end
+
+  get '/portfolio' do
+    erb :portfolio 
+  end
+
+  get '/contact' do
+    erb :contact
   end
 
   get "/cute_pictures_of_animals" do
     erb :cute_pictures_of_animals
   end
+
+
   
 end

64de75baa13d37f651229dbf965b4f62b609608b
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-24 00:45:18 -0800

    week 14 post start

--- my_app.rb
+++ my_app.rb
@@ -2,14 +2,18 @@
 
 require 'sinatra'
 require 'yaml'
+require 'json'
+set :json_encoder, :to_json
 
 class MyApp < Sinatra::Base
+  # helpers Sinatra::JSON
 
   before do
    @posts = Dir.glob("views/posts/*.erb").map do |post_name| 
       post_name.split("/").last.slice(0..-5)
     end
    @sorted_posts = meta_data.sort_by {|post, date_hash| date_hash["date"]}.reverse
+
   end
 
   #  @body_class=Dir.glob("views/*.erb").map do |post_name| 
@@ -29,7 +33,11 @@ class MyApp < Sinatra::Base
   get '/about' do
     @body_class = "about"
     erb :about
+  end
 
+  get '/about.json' do
+    content_type :json
+    {key:"hello"}.to_json
   end
 
   get '/portfolio' do
@@ -54,8 +62,19 @@ class MyApp < Sinatra::Base
   get "/blog/:post_name" do
     @body_class = "post_body"
     page = erb("/posts/#{params[:post_name]}".to_sym, layout: false)
+    p page
     page_body = page.split("\n\n", 2).last
     erb page_body
+  end
+
+  get "/blog/:post_name.:format?",  :provides => [:json] do
+    content_type :json
+    # {hello: "blog"}.to_json
+    # page = erb("/posts/#{params[:post_name]}".to_sym, layout: false)
+    page_hash = {}
+    page_hash[:body] = "hello"
+    p page_hash
+    page_hash.to_json
   end
 
   def meta_data
@@ -64,10 +83,10 @@ class MyApp < Sinatra::Base
     else 
       @metadata = {}
       @posts.each do |post|
-      html = erb("/posts/#{post}".to_sym, layout: false) #returns text of post as a string (html)
-      meta = YAML.load(html.split("\n\n", 2).first) #returns hash, keys are post names, values are hashes with dates
-      @metadata[post] = meta #puts hash into hash
-    end
+        html = erb("/posts/#{post}".to_sym, layout: false) #returns text of post as a string (html)
+        meta = YAML.load(html.split("\n\n", 2).first) #returns hash, keys are post names, values are hashes with dates
+        @metadata[post] = meta #puts hash into hash
+      end
       @metadata
     end
   end
